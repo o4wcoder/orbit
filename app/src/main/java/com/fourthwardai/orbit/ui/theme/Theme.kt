57bccb25393dba7b65e8a41780ca99520c7b5fa1
Chris Hare <chareo4w@yahoo.com>
2025-11-26 14:53:11 -0500

    make bottom system bar match color of nav bar

--- app/src/main/java/com/fourthwardai/orbit/ui/theme/Theme.kt
+++ app/src/main/java/com/fourthwardai/orbit/ui/theme/Theme.kt
@@ -1,4 +1,5 @@
 package com.fourthwardai.orbit.ui.theme
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,9 +10,13 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -277,6 +282,16 @@ fun OrbitTheme(
 
         darkTheme -> darkScheme
         else -> lightScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            @Suppress("DEPRECATION")
+            window.navigationBarColor = colorScheme.surfaceContainer.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
