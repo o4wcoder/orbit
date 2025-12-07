5380c8e53f1a33e4360ed388fb14b5973d0fdb06
Chris Hare <chareo4w@yahoo.com>
2025-12-07 12:24:18 -0500

    remove sorting since it wasn't working correctly

--- app/src/main/java/com/fourthwardai/orbit/repository/ArticleRepositoryImpl.kt
+++ app/src/main/java/com/fourthwardai/orbit/repository/ArticleRepositoryImpl.kt
@@ -2,12 +2,10 @@ package com.fourthwardai.orbit.repository
 
 import com.fourthwardai.orbit.domain.Article
 import com.fourthwardai.orbit.domain.Category
-import com.fourthwardai.orbit.domain.shuffleKey
 import com.fourthwardai.orbit.network.ApiResult
 import com.fourthwardai.orbit.service.newsfeed.ArticleService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import java.time.ZoneOffset
 import javax.inject.Inject
 
 class ArticleRepositoryImpl @Inject constructor(
@@ -25,16 +23,6 @@ class ArticleRepositoryImpl @Inject constructor(
         return when (val result = service.fetchArticles()) {
             is ApiResult.Success -> {
                 _articles.value = result.data
-                    .sortedWith(
-                        compareByDescending<Article> {
-                            // group by calendar day
-                            it.ingestedAt.atZone(ZoneOffset.UTC).toLocalDate()
-                        }.thenBy {
-                            // randomized but stable within the day
-                            it.shuffleKey()
-                        },
-                    )
-
                 _isRefreshing.value = false
                 ApiResult.Success(Unit)
             }
